a1342ad8d370448057cfed4459b4fda6d1f30eca
Luca Montaigut <lmontaigut@gmail.com>
2020-04-18 20:08:51 +0200

    trivia

--- README.md
+++
@@ -1,6 +0,0 @@
-# FilmFinder
-
-## Visit online : https://luca-montaigut.github.io/
-
-## 🐰 Author
-Luca Montaigut : https://github.com/luca-montaigut
--- js/index.js
+++ js/index.js
@@ -1,133 +1,154 @@
-let token;
-let submit = document.getElementById("submit");
-let api = document.getElementById("apikey");
-let selector = document.getElementById("movie");
+let URL;
+let title = document.querySelector("h1");
+let insert = document.getElementById("quizz");
+let next = document.querySelector(".btn");
+let quizz;
+let answers = new Array();
+let userAnswers = new Array();
+let userResults = new Array();
+let userPoints = 0;
+let questionNumber = 0;
 
-const showMore = (code, plot) => {
-  let modal = document.getElementById(`${code}`);
-  document.querySelector(`#${code} .plot`).innerHTML = plot;
-  modal.style.display = "block";
-  //close modal with close-cross or with a click on outside of it
-  let span = document.querySelector(`#${code} .close`).firstChild;
-  span.addEventListener("click", () => {
-    modal.style.display = "none";
+const animate = (move) =>
+  anime({
+    targets: "#qblock",
+    translateX: move,
+    duration: 400,
+    easing: "linear",
   });
-  window.addEventListener("click", () => {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  });
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 };
 
-const readMore = (film) => {
-  console.log(film);
-  let code = film.target.dataset.code;
-  const URL_MOVIE = `https://www.omdbapi.com/?apikey=${token}&i=${code}`;
-  fetch(URL_MOVIE)
+const getQuizz = () => {
+  fetch(URL)
     .then((response) => response.json())
     .then((response) => {
-      showMore(code, response.Plot);
+      quizz = response.results;
+      quizz.forEach((question) => {
+        answers.push(question.correct_answer);
+      });
+      displayQuestion();
     })
     .catch((error) => console.error(error));
 };
 
-const showMovie = (selector, name, year, poster, code) => {
-  selector.innerHTML += `
-    <div class="row border">
-      <div class="col-3 d-flex justify-content-center align-items-center">  
-      <img class="small-img" src="${poster}" alt="">
-      </div>
-      <div class="col-9 d-flex flex-column justify-content-center">
-        <h2>${name.toUpperCase()}</h2>
-        <p><b>${year}</b></p>
-        <div>
-          <input type="submit" data-code="${code}" value="Read more" class="btn btn-info">
+const displayQuestion = () => {
+  title.innerHTML = `Question ${questionNumber + 1} / ${quizz.length}`;
+  let currentQuestion = quizz[questionNumber];
+  insert.innerHTML = "";
+  animate([-2000, 0]);
+  insert.innerHTML = `<h2 class="mb-3">${currentQuestion.question}</h2>`;
+  if (currentQuestion.type === "boolean") {
+    insert.innerHTML += `<br>  
+      <div class='row' style="height: 30vh;">
+        <div class='col d-flex flex-column'>
+          <p class='my-4 btn btn-outline-danger'>True</p>
         </div>
-      </div>
-    </div>
-    <!-- The Modal -->
-    <div id="${code}" class="modal">
-      <div class="modal-content">
-        <span class="close"><i class="far fa-times-circle"></i></span>
-        <div class="row">
-          <div class="col-3 d-flex justify-content-center align-items-center">  
-            <img src="${poster}" alt="">
-          </div>
-          <div class="col-9 d-flex flex-column justify-content-center">
-            <h2><b>${name.toUpperCase()}</b></h2>
-            <p>${year}</p>
-            <p class="plot"></p>
-          </div>
+        <div class='col d-flex flex-column'>
+          <p class='my-4 btn btn-outline-danger'>False</p>
         </div>
+      </div>`;
+  }
+  if (currentQuestion.type === "multiple") {
+    let multiple = [
+      currentQuestion.correct_answer,
+      currentQuestion.incorrect_answers[0],
+      currentQuestion.incorrect_answers[1],
+      currentQuestion.incorrect_answers[2],
+    ];
+    shuffleArray(multiple);
+    insert.innerHTML += `  <br>  
+    <div class='row' style="height: 30vh;">
+      <div class='col d-flex flex-column'>
+        <p class="my-2 btn btn-outline-danger">${multiple[0]}</p>
+        <p class="my-2 btn btn-outline-danger">${multiple[1]}</p>
       </div>
-    </div> 
-`;
+      <div class='col d-flex flex-column'>
+        <p class="my-2 btn btn-outline-danger">${multiple[2]}</p>
+        <p class="my-2 btn btn-outline-danger">${multiple[3]}</p>
+      </div>
+    </div>`;
+  }
+
+  document.querySelectorAll(".btn-outline-danger").forEach((userAnswers) => {
+    userAnswers.addEventListener("click", selectAnswer);
+  });
 };
 
-const searchMovies = () => {
-  selector.innerHTML = "";
-  if (token == null) {
-    alert("Please enter a valid API Key first");
-    return false;
-  }
-  let finder = document.getElementById("findmovie").value;
-  let URL = `https://www.omdbapi.com/?apikey=${token}&s=${finder}`;
-  fetch(URL)
-    .then((response) => response.json())
-    .then((response) => {
-      selector.innerHTML = "";
-      response.Search.forEach((response) => {
-        showMovie(
-          selector,
-          response.Title,
-          response.Year,
-          response.Poster,
-          response.imdbID
-        );
-      });
-      document.querySelectorAll("#movie input").forEach((readmore) => {
-        readmore.addEventListener("click", readMore);
-      });
-    })
-    .then(() => look())
-    .catch((error) => {
-      console.error(error);
-      selector.innerHTML += `
-        <div class="text-center mt-5">
-          <b>No movie fill your reseach, please try something else</b>
-        </div>`;
-    });
+const selectAnswer = (e) => {
+  userAnswers.push(e.target.innerHTML);
+  questionNumber += 1;
+  animate([0, 2000]);
+  setTimeout(() => {
+    if (questionNumber >= quizz.length) {
+      return endGame();
+    }
+    displayQuestion();
+  }, 400);
 };
 
-submit.addEventListener("click", searchMovies);
-
-const getApi = () => {
-  token = prompt("Please enter your API Key from OMDb");
-  if (token == null) {
-    return false;
+const endGame = () => {
+  gameResult();
+  title.innerHTML = `Your result : ${userPoints}/${quizz.length}`;
+  setTimeout(() => {
+    document.getElementById("qblock").style.transform = "translateX(0px)";
+  }, 50);
+  insert.innerHTML = `
+  <table class="table table-bordered">
+    <thead class="thead-light">
+      <th>Question</th>
+      <th>Answer</th>
+      <th>Your answer</th>
+      <th>Result</th>
+    </thead>
+    <tbody  id="result">
+    </tbody>
+  </table>`;
+  for (let i = 0; i < quizz.length; i++) {
+    console.log(i);
+    document.getElementById("result").innerHTML += `
+    <tr>
+      <td>${quizz[i].question}</td>
+      <td>${answers[i]}</td>
+      <td>${userAnswers[i]}</td>
+      <td>${userResults[i]}</td>
+    </tr>
+    `;
   }
-  let URL = `https://www.omdbapi.com/?apikey=${token}&s=batman`;
-  fetch(URL)
-    .then((response) => response.json())
-    .then((response) => {
-      selector.innerHTML = "";
-      response.Search.forEach((response) => {
-        console.log("API Key Valid");
-      });
-      alert("Valid API Key, you can use seach engine now");
-    })
-    .catch((error) => {
-      console.error(error);
-      alert("Wrong API key");
-      token = null;
-    });
 };
 
-apikey.addEventListener("click", getApi);
-
-// Lauch seach on Enter keypress in the form
-document.querySelector(".form-control").addEventListener("keypress", (e) => {
-  if (e.code == "Enter") {
-    searchMovies();
+const gameResult = () => {
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === userAnswers[i]) {
+      userResults.push("<i class='far fa-check-circle'></i>");
+      userPoints += 1;
+    } else {
+      userResults.push("<i class='far fa-times-circle'></i>");
+    }
   }
-});
+};
+
+const howManyQuestion = () => {
+  title.innerHTML = `Trivia THP Next`;
+  insert.innerHTML = ``;
+  insert.innerHTML = `
+  <h3 class="text-center">How many question(s) do you want to answer ?</h3>
+  <div class="form-inline justify-content-center">
+    <input type="number" class="form-control pl-5" value="5">
+    <p id="launch" class="mx-5 my-5 btn btn-danger btn-lg">Go !</p>  
+  </div>
+  `;
+  document.getElementById("launch").addEventListener("click", () => {
+    chosenNumber = document.querySelector("input").value;
+    URL = `https://opentdb.com/api.php?amount=${chosenNumber}`;
+    getQuizz();
+  });
+};
+
+howManyQuestion();
