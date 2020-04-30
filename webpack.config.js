61678e1cb2ca33ea8b7877c75a91ee7576b9c2d8
Luca Montaigut <lmontaigut@gmail.com>
2020-04-30 21:36:17 +0200

    test

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
+
+module.exports = {
+  watch: true,
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "images",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "fonts",
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new Dotenv(),
+    new MiniCssExtractPlugin({
+      filename: "bundle.css",
+    }),
+    new MomentLocalesPlugin({
+      localesToKeep: ["fr"],
+    }),
+  ],
+};
